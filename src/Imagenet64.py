c2590e65ca9363f9b7597bb1b7235bd43bf1983a
Robert Kansy <kansy.robert@web.de>
2021-02-17 18:08:17 +0100

    Fixed imagenet dataset

--- src/Imagenet64.py
+++ src/Imagenet64.py
@@ -16,7 +16,7 @@ random.seed(seed)
 
 class Imagenet64(Dataset):
 
-    def __init__(self, root, class_indices, train=True):
+    def __init__(self, root, class_indices, train=True, transform=None):
         """
         Args:
             root: root to imagenet train_data_batch_(1-10) and val_data
@@ -27,8 +27,7 @@ class Imagenet64(Dataset):
         self.root = root
         self.class_indices = class_indices
         self.images, self.labels = self.get_data()
-
-        self.transform = self.get_transform()
+        self.transform = transform
 
     def __len__(self):
 
@@ -36,22 +35,23 @@ class Imagenet64(Dataset):
 
     def __getitem__(self, idx):
 
-        img = Image.fromarray((self.images[idx] * 255).astype(np.uint8))
+        img = Image.fromarray(self.images[idx].astype(np.uint8))
+        if self.transform is not None:
+            img = self.transform(img)
+        else:
+            img = transforms.ToTensor()(img)
 
-        img = self.transform(img)
-        labels = np.long(self.labels[idx])
-        return img, labels
+        return img, np.long(self.class_indices.index(self.labels[idx]))
 
     def get_data(self):
 
         if self.train:
             print(f'Preparing data batches ({1}/10)')
             con_data, con_labels = self.load_databatch(1, 64)
-            print(f'Data Shape:{con_data.shape}')
 
-            for i in range(1, 10):
-                print(f'Preparing data batches ({i + 1}/10)')
-                data, labels = self.load_databatch(i + 1, 64)
+            for i in range(2, 11):
+                print(f'Preparing data batches ({i}/10)')
+                data, labels = self.load_databatch(i, 64)
                 con_data = np.concatenate((con_data, data), axis=0)
                 con_labels = np.concatenate((con_labels, labels), axis=0)
 
@@ -59,6 +59,46 @@ class Imagenet64(Dataset):
 
         else:
             return self.get_val_data(64)
+
+    def load_databatch(self, idx, img_size=64):
+
+        data_file = os.path.join(self.root, 'train_data_batch_')
+
+        d = unpickle(data_file + str(idx))
+        x = d['data']
+        y = d['labels']
+
+        data_size = 0
+
+        indices = []
+        for i, label in enumerate(y):
+            if label in self.class_indices:
+                data_size += 1
+                indices.append(i)
+
+        labels_cut = []
+        x_cut = np.ndarray(shape=(data_size, x.shape[1]))
+
+        x_idx = 0
+        for idx in indices:
+            x_cut[x_idx] = x[idx]
+            labels_cut.append(y[idx])
+            x_idx += 1
+
+        img_size2 = img_size * img_size
+
+        x_cut = np.dstack((x_cut[:, :img_size2], x_cut[:, img_size2:2 * img_size2], x_cut[:, 2 * img_size2:]))
+        x_cut = x_cut.reshape((x_cut.shape[0], img_size, img_size, 3))
+
+        # create mirrored images
+        x_train = x_cut[0:data_size, :, :, :]
+        y_train = labels_cut[0:data_size]
+        x_train_flip = x_train[:, :, :, ::-1]
+        y_train_flip = y_train
+        x_train = np.concatenate((x_train, x_train_flip), axis=0)
+        y_train = np.concatenate((y_train, y_train_flip), axis=0)
+
+        return x_train, y_train
 
     def get_val_data(self, img_size):
 
@@ -68,7 +108,6 @@ class Imagenet64(Dataset):
         x = d['data']
         y = d['labels']
 
-        x = x.astype('float32') / np.float32(255)
         data_size = 0
 
         indices = []
@@ -86,9 +125,6 @@ class Imagenet64(Dataset):
             labels_cut.append(y[idx])
             x_idx += 1
 
-        # make labels start at 0 instead of 1
-        labels_cut = [j - 1 for j in labels_cut]
-
         img_size2 = img_size * img_size
 
         x_cut = np.dstack((x_cut[:, :img_size2], x_cut[:, img_size2:2 * img_size2], x_cut[:, 2 * img_size2:]))
@@ -103,67 +139,3 @@ class Imagenet64(Dataset):
         y_train = np.concatenate((y_train, y_train_flip), axis=0)
 
         return x_train, y_train
-
-    def load_databatch(self, idx, img_size=64):
-
-        data_file = os.path.join(self.root, 'train_data_batch_')
-
-        d = unpickle(data_file + str(idx))
-        x = d['data']
-        y = d['labels']
-        mean_image = d['mean']
-
-        x = x.astype('float32') / np.float32(255)
-        mean_image = mean_image.astype('float32') / np.float32(255)
-
-        data_size = 0
-
-        indices = []
-        for i, label in enumerate(y):
-            if label in self.class_indices:
-                data_size += 1
-                indices.append(i)
-
-        labels_cut = []
-        x_cut = np.ndarray(shape=(data_size, x.shape[1]))
-
-        x_idx = 0
-        for idx in indices:
-            x_cut[x_idx] = x[idx]
-            labels_cut.append(y[idx])
-            x_idx += 1
-
-        # make labels start at 0 instead of 1
-        labels_cut = [j - 1 for j in labels_cut]
-
-        x_cut -= mean_image
-        img_size2 = img_size * img_size
-
-        x_cut = np.dstack((x_cut[:, :img_size2], x_cut[:, img_size2:2 * img_size2], x_cut[:, 2 * img_size2:]))
-        x_cut = x_cut.reshape((x_cut.shape[0], img_size, img_size, 3))
-
-        # create mirrored images
-        x_train = x_cut[0:data_size, :, :, :]
-        y_train = labels_cut[0:data_size]
-        x_train_flip = x_train[:, :, :, ::-1]
-        y_train_flip = y_train
-        x_train = np.concatenate((x_train, x_train_flip), axis=0)
-        y_train = np.concatenate((y_train, y_train_flip), axis=0)
-
-        return x_train, y_train
-
-    def get_transform(self):
-
-        train_transform = transforms.Compose([
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomCrop(64),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
-
-        val_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
-
-        return train_transform if self.train else val_transform
